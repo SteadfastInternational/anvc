a37c651fab65e08b1709fcee513ff3e19c9ce0e5
SteadfastInternational <hope08136@gmail.com>
2025-01-22 11:36:20 -0800

    sfd

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
-// Remove or comment out the PWA import and configuration
-// const withPWA = require('next-pwa')({
-//   dest: 'public', // The directory where the service worker will be generated
-//   register: true, // Automatically register the service worker
-//   skipWaiting: true, // Skip waiting for the service worker to activate
-// });
-
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Enable static HTML export
   images: {
     remotePatterns: [
       {
@@ -19,6 +13,5 @@ const nextConfig = {
   },
 };
 
-// Export the configuration without PWA settings
-// module.exports = withPWA(nextConfig);
-module.exports = nextConfig; // Just export the nextConfig
+// Export the configuration
+module.exports = nextConfig;
